cbdcb0edfd8d17b1b56a2cce6eb88a80c3528949
linYan <737271441@qq.com>
2022-11-16 09:36:48 +0800

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
     },
   },
   server: {
-    proxy: { "/": "http://localhost:3000" },
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
