c7d15b408c0448b0f7e80481d48cbe43df11ac23
linYan <737271441@qq.com>
2023-01-17 21:47:33 +0800

    story-1 : change login page

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { Root } from "@template";
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById("yroot") as HTMLElement).render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
--- src/pages/accountPage/index.tsx
+++ src/pages/accountPage/index.tsx
@@ -1,31 +1,52 @@
 import "./accountPage.scss";
 import React, { useState } from "react";
 import { Login, Register } from "@components";
+import { LoginOutlined } from "@ant-design/icons";
 
 export const AccountPage = () => {
-  const [play, setPlay] = useState(true);
+  const [selectSection, setselectSection] = useState(false);
 
   return (
-    <div className="certification">
-      <div className="switch-group">
-        <div
-          className={`login-switch  switch-item ${play ? "" : "pause"}  `}
-          onMouseEnter={() => setPlay(false)}
-          onMouseLeave={() => setPlay(true)}
-        >
+    <div className="certification ">
+      <div className="left-login">
+        <div className="top-info">
+          <div className="info-tips">
+            <span className="text-h1 text-cap">don't have an account yet?</span>
+          </div>
+          <div className="arrow flex-center">
+            <LoginOutlined />
+          </div>
+        </div>
+        <Login />
+      </div>
+
+      <div className="switch-group flex-center flex-h">
+        <div className={`login-switch  switch-item`}>
           <span className="login-text text-up text">
             <p>log in</p>
           </span>
         </div>
-        <div className="creat-account-switch switch-item">
+
+        <div className="creat-account-switch switch-item pass">
           <span className="create-text text-up text">
             <p>create</p>
           </span>
         </div>
       </div>
 
-      {/* <Login /> */}
-      {/* <Register /> */}
+      <div className="right-register ">
+        <Register />
+        <div className="top-info">
+          <div className="info-tips">
+            <span className="text-h1 text-cap">
+              Yes I already have an account.{" "}
+            </span>
+          </div>
+          <div className="arrow flex-center">
+            <LoginOutlined />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
