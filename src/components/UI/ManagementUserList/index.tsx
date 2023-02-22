6cc8e62ec1cc95f8cd06fc1500125a6e127259dc
linYan <737271441@qq.com>
2023-02-22 18:22:37 +0800

    add tables form

--- src/components/UI/ManagementUserList/index.tsx
+++ src/components/UI/ManagementUserList/index.tsx
@@ -1,7 +1,83 @@
 import React from "react";
-export interface ManagementUserList {}
-[];
+import { Space, Table, Tag } from "antd";
+import type { ColumnsType } from "antd/es/table";
 
-export const ManagementUserList = () => {
-  return <div className="management-list">ManagementUserList</div>;
+export interface ManagementUserItem {
+  key: string;
+  name: string;
+  telnumber: number;
+  password: string;
+  createDate: string;
+}
+export interface ManagementUserList {
+  userList: ManagementUserItem[];
+}
+
+export const ManagementUserList: React.FC<ManagementUserList> = ({
+  userList,
+}) => {
+  const columns: ColumnsType<ManagementUserItem> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "TelNumber",
+      dataIndex: "telnumber",
+      key: "telnumber",
+    },
+    {
+      title: "password",
+      dataIndex: "password",
+      key: "password",
+    },
+    {
+      title: "CreateDate",
+      key: "createDate",
+      dataIndex: "createDate",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
+  const data: ManagementUserItem[] = [
+    {
+      key: "1",
+      name: "John Brown",
+      password: "New York No. 1 Lake Park",
+      telnumber: 1222222,
+      createDate: "2",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      telnumber: 666666666,
+      password: "London No. 1 Lake Park",
+      createDate: "1677032212724",
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      createDate: "1677032212724",
+      telnumber: 7777777777777,
+      password: "Sydney No. 1 Lake Park",
+    },
+  ];
+
+  return (
+    <div className="management-list">
+      <Table dataSource={data} columns={columns} />
+      <Table></Table>
+    </div>
+  );
 };
